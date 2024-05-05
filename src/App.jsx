01d7e4474b8057e18c2a9a336820ea7f336357e9
JIB2017 <blacuttjuan07@gmail.com>
2024-05-05 17:37:54 -0300

    Implementing initial components & their routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
-// import "./App.css"
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from "./components";
+import "./App.css"
 
 const App = () => {
   return (
@@ -11,8 +11,28 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+            Crypto Planet <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
